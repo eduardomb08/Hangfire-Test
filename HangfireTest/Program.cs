dc5a5f845c00afb96897567f52c228b93931f9a1
Eduardo Monteiro de Barros <eduardo.mb@amwins.com>
2019-04-01 17:01:51 -0400

    Initial commit

--- HangfireTest/Program.cs
+++ HangfireTest/Program.cs
@@ -1,15 +1,99 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Hangfire;
+using Microsoft.Owin.Hosting;
+using Ninject;
+using Serilog;
+using Serilog.Core;
+using System;
+using System.Diagnostics;
+using System.Threading;
 
 namespace HangfireTest
 {
     class Program
     {
+        private static IKernel _kernel = new StandardKernel();
+
         static void Main(string[] args)
         {
+            using (WebApp.Start<Startup>("http://localhost:9000"))
+            {
+                //RecurringJob.AddOrUpdate(() => Console.WriteLine("Hangfire Works"), Cron.Minutely);
+                RunTest();
+
+                Console.WriteLine("Hangfire on");
+                Console.ReadKey();
+            }
+
+            //SetupLogger();
+            //SetupHangfire();
+
+            //var act = CreateActivator();
+            //var logger = act.ActivateJob(typeof(Logger));
+
+            //using (var server = new BackgroundJobServer())
+            //{
+            //    RunTest();
+
+            //    Console.WriteLine("Press any key to exit...");
+            //    Console.ReadKey();
+            //}
+        }
+
+        //private static NinjectJobActivator CreateActivator()
+        //{
+        //    return new NinjectJobActivator(_kernel);
+        //}
+
+        private static void RunTest()
+        {
+            int max = 3;
+
+            Log.Logger.Information($"Starting batch job ...");
+            var bid = BatchJob.StartNew(x =>
+            {
+                for (int i = 0; i < max; i++)
+                {
+                    Log.Logger.Information($"Enqueing a new job for {i}");
+                    x.Enqueue(() => DoLongWork(i));
+                }
+            });
+
+            Log.Logger.Information($"Waiting for {max} jobs to finish...");
+            var wid = BatchJob.AwaitBatch(bid, y =>
+            {
+                y.Enqueue(() => Log.Logger.Information("All jobs done."));
+            });
+        }
+
+        //private static void SetupHangfire()
+        //{
+        //    _kernel.Bind<ILogger>().To<Logger>();
+        //    _kernel.Bind<Logger>().ToConstant(GetLogger());
+
+        //    GlobalConfiguration.Configuration.UseNinjectActivator(_kernel);
+
+        //    GlobalConfiguration.Configuration.UseSqlServerStorage("_DBCONNECT.LOCAL_HANGFIRESTORAGE");
+        //    GlobalConfiguration.Configuration.UseBatches();
+        //}
+
+        //private static void SetupLogger()
+        //{
+        //    Log.Logger = GetLogger();
+        //}
+
+        //private static Logger GetLogger()
+        //{
+        //    return new LoggerConfiguration()
+        //        .WriteTo.Console()
+        //        .WriteTo.File($@"C:\Users\cruzedu\Downloads\hangfire_{Process.GetCurrentProcess().Id}.txt")
+        //        .CreateLogger();
+        //}
+
+        public static void DoLongWork(int id)
+        {
+            Log.Logger.Information($"Starting job {id}...");
+            Thread.Sleep(2000);
+            Log.Logger.Information($"Job {id} finished.");
         }
     }
 }
---
+++ HangfireTest/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Hangfire;
+using Microsoft.Owin;
+using Ninject;
+using Owin;
+using Serilog;
+using Serilog.Core;
+
+[assembly: OwinStartup(typeof(HangfireTest.Startup))]
+
+namespace HangfireTest
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            GlobalConfiguration.Configuration.UseSqlServerStorage("_DBCONNECT.LOCAL_HANGFIRESTORAGE");
+            GlobalConfiguration.Configuration.UseBatches();
+
+            var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Log.Logger = logger;
+
+            var kernel = new StandardKernel();
+            kernel.Bind<ILogger>().To<Logger>();
+            kernel.Bind<Logger>().ToConstant(logger);
+
+            GlobalConfiguration.Configuration.UseNinjectActivator(kernel);
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+        }
+    }
+}
